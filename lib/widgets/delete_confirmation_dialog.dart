2ba81e56d830c6a5c965b889d74718189d7c17bc
Hp <bsc_43_21@unima.ac.mw>
2026-03-21 03:24:35 -0700

    showDialog + StatefulBuilder

--- lib/widgets/delete_confirmation_dialog.dart
+++ lib/widgets/delete_confirmation_dialog.dart
@@ -6,3 +6,9 @@ import '../services/weather_smart_service.dart';
 Future<void> showDeleteConfirmationDialog(BuildContext context, {required PlotModel plot}) async {
   final controller = TextEditingController();
   bool isSaving = false;
+    return showDialog(
+    context: context,
+    builder: (context) {
+      return StatefulBuilder(
+        builder: (context, setDialogState) {
+          final bool isMatch = controller.text.trim() == plot.name;
