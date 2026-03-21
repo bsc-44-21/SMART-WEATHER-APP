5dab7a8fc5cda1a3761ec114bf8c9a4d107e425f
brightchimwaza <bsc-06-21@unima.ac.mw>
2026-03-21 11:47:18 +0200

    feat: create profile settings page layout

---
+++ lib/screens/profile_settings_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:lucide_icons/lucide_icons.dart';
+import 'package:provider/provider.dart';
+import '../widgets/common_widgets.dart';
+import '../core/theme.dart';
+import '../services/weather_smart_service.dart';
+import '../services/auth_service.dart';
+import 'auth_screen.dart';
+
+class ProfileSettingsPage extends StatelessWidget {
+  const ProfileSettingsPage({super.key});
