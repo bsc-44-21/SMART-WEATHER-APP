b883a8825c195c0dc9a862bb422ff6dd553de633
Hp <bsc_43_21@unima.ac.mw>
2026-03-21 02:47:14 -0700

    imports for log-in page

---
+++ lib/screens/auth_screen.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:lucide_icons/lucide_icons.dart';
+import 'package:provider/provider.dart';
+import '../widgets/common_widgets.dart';
+import '../services/auth_service.dart';
+import 'dashboard_screen.dart';
+import 'sign_up_screen.dart';
