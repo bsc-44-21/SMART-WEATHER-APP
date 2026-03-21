39b2c503ffe4ba3531219f4229d6fbf707a39a64
FaraiWona <Bsc-04-21@unima.ac.mw>
2026-03-21 02:59:26 -0700

    weatherSmartService

--- lib/services/weather_smart_service.dart
+++ lib/services/weather_smart_service.dart
@@ -5,3 +5,9 @@ import '../core/mock_data.dart';
 import '../models/plot.dart';
 import 'firestore_service.dart';
 import 'package:intl/intl.dart';
+
+class WeatherSmartService extends ChangeNotifier {
+  List<PlotModel> _plots = [];
+  final List<Map<String, dynamic>> _activities = List.from(MockData.activities);
+  bool _isDarkMode = false;
+  StreamSubscription? _plotsSubscription;
