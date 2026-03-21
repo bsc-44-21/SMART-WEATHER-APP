95d1499033e5aef1c284399b8049f46f2419a56f
Hp <bsc_43_21@unima.ac.mw>
2026-03-21 03:09:06 -0700

    State Variables

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -2,3 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 class AuthService extends ChangeNotifier {
   final FirebaseAuth _auth = FirebaseAuth.instance;
+    // State
+  User? _user;
+  bool _isLoading = false;
+  String? _errorMessage;
