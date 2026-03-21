18f8d966dfcaf01e20458a59cb8b37a7b3a12267
FaraiWona <Bsc-04-21@unima.ac.mw>
2026-03-21 03:15:11 -0700

    farmingcard class

--- lib/widgets/common_widgets.dart
+++ lib/widgets/common_widgets.dart
@@ -2,3 +2,8 @@ mport 'package:flutter/material.dart';
 import 'package:lucide_icons/lucide_icons.dart';
 import '../core/theme.dart';
 import '../models/plot.dart';
+
+class FarmingCard extends StatelessWidget {
+  final Widget child;
+  final EdgeInsetsGeometry? padding;
+  final VoidCallback? onTap;
