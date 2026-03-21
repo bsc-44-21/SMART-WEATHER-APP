e379c250720bb0ef09786443aaa466fe3f4a8248
brightchimwaza <bsc-06-21@unima.ac.mw>
2026-03-21 12:22:46 +0200

    add delete confirmation dialog for plots

---
+++ lib/widgets/create_plot_sheet.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:intl/intl.dart';
+import '../services/weather_smart_service.dart';
+import '../services/auth_service.dart';
+import '../models/plot.dart';
+
+void showCreatePlotBottomSheet(BuildContext context, {PlotModel? existingPlot}) {
+  final nameController = TextEditingController(text: existingPlot?.name);
+  final locationController = TextEditingController(text: existingPlot?.location);
+  final sizeController = TextEditingController(text: existingPlot?.size);
+  final formKey = GlobalKey<FormState>();
