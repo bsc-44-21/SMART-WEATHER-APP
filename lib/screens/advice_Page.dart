b812dd97a2488e77f6bb240ebd04b7cdae94e3e2
FaraiWona <Bsc-04-21@unima.ac.mw>
2026-03-21 02:40:17 -0700

    advice page class

--- lib/screens/advice_Page.dart
+++ lib/screens/advice_Page.dart
@@ -4,3 +4,9 @@ import 'package:provider/provider.dart';
 import '../widgets/common_widgets.dart';
 import '../services/weather_smart_service.dart';
 
+class AdvicePage extends StatefulWidget {
+  const AdvicePage({super.key});
+
+  @override
+  State<AdvicePage> createState() => _AdvicePageState();
+}
