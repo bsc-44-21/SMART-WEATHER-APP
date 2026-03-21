ed7680ccc841c006af3554a523643a6c27a0e4a9
Hassan Zamin <bsc-44-21@unima.ac.mw>
2026-03-21 10:58:40 +0200

    Started implemented the home screen

---
+++ lib/screens/home_page.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:lucide_icons/lucide_icons.dart';
+import 'package:provider/provider.dart';
+import '../widgets/common_widgets.dart';
+import '../core/theme.dart';
+import '../services/weather_smart_service.dart';
+import '../widgets/create_plot_sheet.dart';
+import '../widgets/delete_confirmation_dialog.dart';
