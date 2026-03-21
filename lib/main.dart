d68b3d197464d56172f1e2c9073c641ebd710b07
Hassan Zamin <bsc-44-21@unima.ac.mw>
2026-03-21 12:06:39 +0200

    Added all imports needed

---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'core/theme.dart';
+import 'screens/auth_screen.dart';
+import 'services/weather_smart_service.dart';
+import 'services/auth_service.dart';
