9bfd56bb69554f4fb7df85ec373f1b72952cf540
FaraiWona <Bsc-04-21@unima.ac.mw>
2026-03-24 15:21:08 -0700

    get permission of location cordinates

--- lib/services/weather_location_service.dart
+++ lib/services/weather_location_service.dart
@@ -1,3 +1,7 @@
 import 'package:geolocator/geolocator.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+
+class WeatherLocationService {
+
+  static Future<Position?> getLocationWithPermission() async {
