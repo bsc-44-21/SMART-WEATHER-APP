d248d8ad83c5dcdd33e1b33f2c27674c6c082bca
FaraiWona <Bsc-04-21@unima.ac.mw>
2026-04-06 10:16:04 -0700

    declaring variable

--- lib/models/activity_log.dart
+++ lib/models/activity_log.dart
@@ -1,1 +1,11 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+
+class ActivityLogModel {
+  final String id;
+  final String userId;
+  final String plot;
+  final String title;
+  final String time;
+  final bool? isRecommended;
+  final String? aiFeedback;
+  final DateTime createdAt;
