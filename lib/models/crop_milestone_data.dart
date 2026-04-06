a3e4d6f844b6a7a3391e5aa591a6ac5b5d0f18c8
FaraiWona <Bsc-04-21@unima.ac.mw>
2026-04-06 09:56:16 -0700

    declaring activity 2

--- lib/models/crop_milestone_data.dart
+++ lib/models/crop_milestone_data.dart
@@ -9,4 +9,9 @@ class CropMilestoneData {
           'title': 'First Weeding',
           'body': 'Your Maize plot "$plotName" is 3 weeks old. Start weeding to prevent nutrient competition.',
         },
+         {
+          'dap': 45,
+          'title': 'Top Dressing (Urea)',
+          'body': 'Time to apply nitrogen fertilizer to "$plotName" for optimal stalk and leaf growth.',
+        },
 }
