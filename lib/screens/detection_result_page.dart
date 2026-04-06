70d6f90c29ec0989c06a76f5a4907bd05585fe8a
brightchimwaza <bsc-06-21@unima.ac.mw>
2026-04-06 18:54:57 +0200

    Create DetectionResultPage UI structure with scaffold and layout

---
+++ lib/screens/detection_result_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:lucide_icons/lucide_icons.dart';
+import 'package:google_fonts/google_fonts.dart';
+import '../core/theme.dart';
+import '../models/pest_detection.dart';
+import '../services/report_service.dart';
+
+class DetectionResultPage extends StatelessWidget {
+  final PestDetectionModel detection;
+
+  const DetectionResultPage({super.key, required this.detection});
+
+  @override
+  Widget build(BuildContext context) {
+    final riskLower = detection.riskLevel.toLowerCase();
+    final isHighRisk = riskLower == 'high';
+    final isMediumRisk = riskLower == 'medium';
+    
+    final Color riskColor = isHighRisk 
+        ? Colors.red 
+        : (isMediumRisk ? Colors.orange : Colors.green);
