137a38c2d74d00c02a347f87378256966cb84dff
FaraiWona <Bsc-04-21@unima.ac.mw>
2026-04-06 09:50:41 -0700

    declaring activity 1

--- lib/models/crop_milestone_data.dart
+++ lib/models/crop_milestone_data.dart
@@ -1,4 +1,12 @@
 class CropMilestoneData {
     static List<Map<String, dynamic>> getMilestonesForCrop(String cropName, String plotName) {
     String normalizedCrop = cropName.toLowerCase().trim();
+
+    if (normalizedCrop.contains('maize')) {
+      return [
+        {
+          'dap': 21,
+          'title': 'First Weeding',
+          'body': 'Your Maize plot "$plotName" is 3 weeks old. Start weeding to prevent nutrient competition.',
+        },
 }
