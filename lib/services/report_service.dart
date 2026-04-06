52f694640616cf000d4dd841d4829450809e0c2e
FaraiWona <Bsc-04-21@unima.ac.mw>
2026-04-06 10:23:48 -0700

    imports

---
+++ lib/services/report_service.dart
@@ -0,0 +1,7 @@
+import 'dart:io';
+import 'package:pdf/pdf.dart';
+import 'package:pdf/widgets.dart' as pw;
+import 'package:printing/printing.dart';
+import 'package:path_provider/path_provider.dart';
+import '../models/pest_detection.dart';
+import 'package:intl/intl.dart';
