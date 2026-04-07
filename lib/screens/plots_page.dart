9dd5cb07eeb2bbf9cc30c2f7e1f65067df88d043
Hassan Zamin <bsc-44-21@unima.ac.mw>
2026-04-07 09:26:35 +0200

    I've updated the "Add Plot" button to a more modern and descriptive style

--- lib/screens/plots_page.dart
+++ lib/screens/plots_page.dart
@@ -160,10 +160,14 @@ class _PlotsPageState extends State<PlotsPage> {
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
+      floatingActionButton: FloatingActionButton.extended(
         onPressed: () => showCreatePlotBottomSheet(context),
         backgroundColor: AppTheme.primaryAccent,
-        child: const Icon(LucideIcons.plus, color: Colors.white),
+        icon: const Icon(LucideIcons.plus, color: Colors.white, size: 20),
+        label: const Text(
+          "Add Plot",
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        ),
       ),
     );
   }
