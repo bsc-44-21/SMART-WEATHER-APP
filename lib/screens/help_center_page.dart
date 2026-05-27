1df41d23c825aee7b47fc62ff9ed096b058f97c4
brightchimwaza <bsc-06-21@unima.ac.mw>
2026-05-27 17:35:06 +0200

    Refactor terms section text styling for consistency

--- lib/screens/help_center_page.dart
+++ lib/screens/help_center_page.dart
@@ -3,3 +3,6 @@ import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:url_launcher/url_launcher.dart';
 import '../core/theme.dart';
+
+class HelpCenterPage extends StatelessWidget {
+  const HelpCenterPage({super.key});
