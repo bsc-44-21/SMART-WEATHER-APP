6bc7afb953c40a43bedca90210258244c9986f19
brightchimwaza <bsc-06-21@unima.ac.mw>
2026-05-27 17:26:48 +0200

    Add scaffold structure for terms and conditions page

--- lib/screens/terms_of_service_page.dart
+++ lib/screens/terms_of_service_page.dart
@@ -4,3 +4,13 @@ import '../core/theme.dart';
 
 class TermsOfServicePage extends StatelessWidget {
   const TermsOfServicePage({super.key});
+@override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+      appBar: AppBar(
+        title: Text('Terms of Reference', style: Theme.of(context).textTheme.titleLarge),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        iconTheme: IconThemeData(color: Theme.of(context).textTheme.bodyLarge?.color),
+      ),
