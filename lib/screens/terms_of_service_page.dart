2caec5bcb3cacb83245ffd70e7b6e33ad3aa1741
brightchimwaza <bsc-06-21@unima.ac.mw>
2026-05-27 17:10:18 +0200

    Created initial PrivacyPolicyPage layout

---
+++ lib/screens/terms_of_service_page.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import '../core/theme.dart';
+
+class PrivacyPolicyPage extends StatelessWidget {
+  const PrivacyPolicyPage({super.key});
+
