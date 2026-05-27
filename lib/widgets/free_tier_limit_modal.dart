d2367a621856deef22838f2227b80d369559ac1e
FaraiWona <bsc-04-21@unima.ac.mw>
2026-05-27 16:28:10 -0700

    initialising free_tier_model.dart

--- lib/widgets/free_tier_limit_modal.dart
+++ lib/widgets/free_tier_limit_modal.dart
@@ -3,3 +3,13 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:lucide_icons/lucide_icons.dart';
 import '../core/theme.dart';
 import '../screens/payment_screen.dart';
+
+void showFreeTierLimitModal(BuildContext context, {required String featureName, required String limitText}) {
+  showDialog(
+    context: context,
+    builder: (context) => _FreeTierLimitModal(
+      featureName: featureName,
+      limitText: limitText,
+    ),
+  );
+}
