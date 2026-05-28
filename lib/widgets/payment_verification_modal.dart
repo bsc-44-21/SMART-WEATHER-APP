0d8746bf0cd459156d89c816ba5916c57e34bf23
FaraiWona <bsc-04-21@unima.ac.mw>
2026-05-28 02:20:20 -0700

    defined variables

--- lib/widgets/payment_verification_modal.dart
+++ lib/widgets/payment_verification_modal.dart
@@ -2,3 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:lucide_icons/lucide_icons.dart';
 import '../core/theme.dart';
+
+class PaymentVerificationModal extends StatelessWidget {
+  final String txRef;
+  final double amount;
+  final VoidCallback onVerify;
+  final VoidCallback onCancel;
+  final bool isLoading;
+  final String? errorMessage;
